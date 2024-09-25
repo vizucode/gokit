c2851b31bbe4b6a632aaa4d75d2fd092e224ba50
Vizucode <havisiqbalsyah@gmail.com>
2024-09-25 11:31:39 +0700

    feat: loader config

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+// Load any configuration like open connection database, open connection redis, monitoring, e.t.c
+func Load(serviceName string) {
+
+	serviceName = strings.ToLower(serviceName)
+	serviceName = strings.ReplaceAll(serviceName, "-", "_")
+	serviceName = strings.ReplaceAll(serviceName, " ", "_")
+
+	// load all configuration needed
+	// init viper first time
+	Config()
+
+	fmt.Println(serviceName)
+}
+
+func Config() {
+	viper.AutomaticEnv()
+	viper.SetConfigFile(".env")
+	viper.SetConfigName("env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println("failed to load env")
+	}
+}
